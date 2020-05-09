09d5f1165d5c5607d961df4e97f74ba9e4d62966
Chandrashekhar Otari <shekhar.timepass@gmail.com>
2020-05-09 13:28:40 +0530

    First commit

---
+++ customer-service/src/main/java/com/example/customer/CustomerDO.java
@@ -0,0 +1,56 @@
+package com.example.customer;
+
+import java.io.Serializable;
+
+public class CustomerDO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private int age;
+
+	public CustomerDO() {
+	}
+
+	public CustomerDO(Long id, String firstName, String lastName, int age) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
---
+++ customer-service/src/main/java/com/example/customer/CustomerServiceController.java
@@ -0,0 +1,50 @@
+package com.example.customer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping(value = "/customer")
+public class CustomerServiceController {
+	
+	private static AtomicLong customerIdGenerator = new AtomicLong(0);
+	
+	private static Map<Long, CustomerDO> customerMap = new ConcurrentHashMap<>();
+	
+	static {
+		customerMap.put(customerIdGenerator.incrementAndGet(), new CustomerDO(customerIdGenerator.get(), "Reshama", "Otari", 35));
+		customerMap.put(customerIdGenerator.incrementAndGet(), new CustomerDO(customerIdGenerator.get(), "Chandrashekhar", "Otari", 39));
+	}
+	
+	@GetMapping(produces = {"application/json"})
+	public ResponseEntity<List<CustomerDO>> getCustomers() {
+		List<CustomerDO> customerDOs = new ArrayList<>();
+		
+		customerMap.forEach((Long customerId, CustomerDO customerDO) -> customerDOs.add(customerDO));
+		
+		ResponseEntity<List<CustomerDO>> responseEntity = new ResponseEntity<>(customerDOs, HttpStatus.OK);
+		return responseEntity;
+	}
+	
+	@GetMapping(value = "/{id}", produces = {"application/json"})
+	public ResponseEntity<CustomerDO> getCustomer(@PathVariable("id") Long customerId) {		
+		CustomerDO customerDO = customerMap.get(customerId);
+		if (customerDO == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer with id " + customerId + " is missing");
+		}
+		
+		ResponseEntity<CustomerDO> responseEntity = new ResponseEntity<>(customerDO, HttpStatus.OK);
+		return responseEntity;
+	}
+}
