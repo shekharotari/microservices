8f651e3731ce46271ed06df23ad4b41d89f0e17d
Chandrashekhar Otari <shekhar.timepass@gmail.com>
2020-05-17 21:06:31 +0530

    Secure Inventory service

--- inventory-service/src/main/java/com/example/inventory/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/example/inventory/InventoryServiceApplication.java
@@ -3,9 +3,11 @@ package com.example.inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @RefreshScope
+@EnableResourceServer
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
---
+++ inventory-service/src/main/java/com/example/inventory/config/ResourceServerConfig.java
@@ -0,0 +1,23 @@
+package com.example.inventory.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+@Configuration
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		super.configure(resources);
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/inventory/**").hasRole("ADMIN").anyRequest().authenticated();
+//		http.authorizeRequests().anyRequest().authenticated();
+	}
+
+}
