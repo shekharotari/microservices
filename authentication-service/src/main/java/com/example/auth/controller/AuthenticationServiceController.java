c782fb401426ac2bf65a957d02eeff55c9e4ad6f
Chandrashekhar Otari <shekhar.timepass@gmail.com>
2020-05-17 19:10:53 +0530

    Protect customer service

--- authentication-service/src/main/java/com/example/auth/controller/AuthenticationServiceController.java
+++ authentication-service/src/main/java/com/example/auth/controller/AuthenticationServiceController.java
@@ -5,11 +5,9 @@ import java.util.Map;
 
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/")
 public class AuthenticationServiceController {
 	
 	@GetMapping(value = "/user", produces = {"application/json"})
--- customer-service/src/main/java/com/example/customer/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/customer/CustomerServiceApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @RefreshScope
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableResourceServer
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
